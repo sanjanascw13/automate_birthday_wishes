f679a03f92808e012caefaf639ea81d9ee83e0b2
sanjanascw13 <73396611+sanjanascw13@users.noreply.github.com>
2026-04-24 15:23:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
 import datetime as dt
 import pandas
 import smtplib
 import os
-
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 to_replace = "[Name]"
 my_mail = os.environ.get("MY_MAIL")
